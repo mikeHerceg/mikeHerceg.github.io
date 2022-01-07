3be81de54a3de44fd776c75da863d4a50dea3051
mikeHerceg <mikehercegdesigns@gmail.com>
2022-01-07 16:24:16 -0500

    adding smooth scroll

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,35 @@
 console.log('Thanks for checking out my log.')
 
+const getHeaderHeight = () => {
+    return document.querySelector('.sticky-header').clientHeight;
+}
+
+
 const adjustBodyForNav = () => {
-    const headerHeight =  document.querySelector('.sticky-header').clientHeight;
+    const headerHeight =  getHeaderHeight();
     const main = document.getElementsByTagName('main')[0];
     main.style.marginTop = (headerHeight + 32) +'px';
 }
 
 const smoothScroll = (e) => {
-    e.preventDefualt()
-    
-//todo
+    e.preventDefault()
+    const targetId = e.target.href.split('#')[1]
+    const targetElement =  document.getElementById(targetId);
+    targetElement.offsetTop = 0;
+    window.scrollTo({
+        top: targetElement.offsetTop - getHeaderHeight(),
+        left:0,
+        behavior: 'smooth'
+    })
+    console.log(targetElement.offsetTop)    
 }
 
-adjustBodyForNav()
+const addEventsHandlers = () => {
+    const internalLinks = document.querySelectorAll("a[href^='#']")
+    internalLinks.forEach(link=>{
+        link.addEventListener('click', e => smoothScroll(e))
+    })
+}
+
+addEventsHandlers();
+adjustBodyForNav();
