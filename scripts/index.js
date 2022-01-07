4a843ae2c87e1c48948486937403b2a00808e2f5
mikeHerceg <mikehercegdesigns@gmail.com>
2022-01-07 12:44:13 -0500

    fixing nav to top and addjusting for height

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
 console.log('Thanks for checking out my log.')
+
+const adjustBodyForNav = () => {
+    const headerHeight =  document.querySelector('.sticky-header').clientHeight;
+    const main = document.getElementsByTagName('main')[0];
+    main.style.marginTop = (headerHeight + 32) +'px';
+    console.log(main)
+}
+
+adjustBodyForNav()
